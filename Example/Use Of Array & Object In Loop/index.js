0cb6c3b8ca4756b8600e9faf9c1a3bcef845d34a
felix204 <icigenaydincan@gmail.com>
2023-02-26 12:42:51 +0300

    Use Of Array & Object In Loop Start

---
+++ Example/Use Of Array & Object In Loop/index.js
@@ -0,0 +1,32 @@
+// let cars = ['BMW', 'Mercedes', 'Audi'];
+
+// for (let i = 0; i < cars.length; i++) {
+//     console.log(cars[i]);
+    
+// }
+
+
+
+
+
+// FOR-IN
+
+// let cars = ['BMW', 'Mercedes', 'Audi'];
+
+// for (const i in cars) {
+//    console.log(`index : ${i}, value : ${cars[i]}`); 
+// }
+
+// console.log(typeof cars);
+
+
+
+
+
+//FOREACH
+
+let cars = ['BMW', 'Mercedes', 'Audi'];
+
+cars.forEach(function(item){
+    console.log(item);
+})
