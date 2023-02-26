73753ae1626c27af4226b49c942f4397bf0ac90d
felix204 <icigenaydincan@gmail.com>
2023-02-26 11:45:53 +0300

    Object Literals Finish

---
+++ Example/Object Literals/index.js
@@ -0,0 +1,41 @@
+let val ;
+// let person = {
+//     firstName : 'Aydın Can',
+//     lastName : 'İçiğen',
+//     age : 24,
+//     hobbies : ['music','drums'],
+//     adress : {
+//         city : 'Çorum',
+//         Country : 'Türkiye' 
+//     },
+//     getBirthYear: function(){
+//         return 2022-this.age ;
+//     }
+// }
+
+// val = person;
+// val = person.firstName;
+// val = person.lastName;
+// val = person.age;
+// val = person.hobbies;
+// val = person.hobbies[1];
+// val = person.adress.city;
+// val = person.adress.Country;
+// val = person.getBirthYear();
+
+let people = [
+    {
+        firstName : 'Aydın Can',
+        lastName : 'İçiğen'
+    },
+    {
+        firstName : 'Özcan',
+        lastName : 'İçiğen'
+    }
+]
+
+val = people[1];
+    
+
+console.log(person)
+console.log(typeof person);
